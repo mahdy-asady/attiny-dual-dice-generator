f9f5070e8a051e17971764c405f6e88a8c61e95d
Mahdy Asady <mahdy.asady@gmail.com>
2022-10-22 04:41:48 +0330

    Function Clean up

--- src/main.c
+++ src/main.c
@@ -4,32 +4,50 @@
 
 
 uint8_t ConvertToDice(uint8_t);
+void Flasher(void);
+void ShowDice(void);
 
-int main(void) {
+void InitSystem(void) {
     //Enable Counter Clock
     TCCR0B |= 1 << CS02;
     //Set All PortB pins to output
-    DDRB = 0xFF;
-    
+    DDRB = _BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3);
+    PORTB |= _BV(PB4); //Set PB4 Input & Pull Up
+
     ShiftRegister_Setup(&PORTB, PB0, PB1, PB2, PB3, 8);
+}
+
+
+int main(void) {
+    InitSystem();
     
-    uint8_t Counter = 0;
+    uint32_t Counter = 0;
     while(1) {
-        Counter++;
-        if(Counter == 1)
+        if(~PINB & _BV(PINB4)) {
+            Counter = 100000;
             ShiftRegister_EnableOutput();
-        else if(Counter == 5)
-            ShiftRegister_DisableOutput();
-        else if(Counter == 10)
-            Counter = 0;
+        }
         
-        uint8_t FirstDice = ConvertToDice((TCNT0 % 6) + 1);
-        uint8_t SecondDice = ConvertToDice(((TCNT0 / 10) % 6) + 1);
-        ShiftRegister_SendByte((SecondDice << 4) | FirstDice);
-        _delay_ms(20);
+        if(Counter == 0) {
+            Flasher();
+            ShowDice();
+            _delay_ms(20);
+        } else {
+            Counter--;
+        }
     }
 }
 
+void Flasher(void) {
+    static uint8_t Counter = 0;
+    Counter++;
+    if(Counter == 1)
+        ShiftRegister_EnableOutput();
+    else if(Counter == 5)
+        ShiftRegister_DisableOutput();
+    else if(Counter == 10)
+        Counter = 0;
+}
 
 uint8_t ConvertToDice(uint8_t Number) {
     uint8_t Result = 0;
@@ -55,4 +73,10 @@ uint8_t ConvertToDice(uint8_t Number) {
         break;
     }
     return Result;
+}
+
+void ShowDice(void)  {
+    uint8_t FirstDice = ConvertToDice((TCNT0 % 6) + 1);
+    uint8_t SecondDice = ConvertToDice(((TCNT0 / 10) % 6) + 1);
+    ShiftRegister_SendByte((SecondDice << 4) | FirstDice);
 }
